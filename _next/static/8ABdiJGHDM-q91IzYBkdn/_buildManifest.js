89f4ff51e336bb514cf1600b901ac3335121ac55
Pratik Kumar <pk@rapidinnovation.dev>
2024-07-11 02:48:30 +0530

    Updates

---
+++ _next/static/8ABdiJGHDM-q91IzYBkdn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/153-f40bc5b3a399c339.js","static/chunks/pages/index-8e1bd884bb107d9b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
