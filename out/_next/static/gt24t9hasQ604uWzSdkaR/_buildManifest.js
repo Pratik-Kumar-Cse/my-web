bafdfbec7a8757b514f49c6a39ac75d7a23e170f
Pratik Kumar <pk@rapidinnovation.dev>
2024-07-11 01:21:58 +0530

    deployment changes

---
+++ out/_next/static/gt24t9hasQ604uWzSdkaR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/399-7b052727187b4754.js","static/chunks/pages/index-3aca5bc7d76c9f42.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
