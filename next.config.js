e5b362a98ca6ee2442c184d8afc087c657a6cdd4
Pratik Kumar <pk@rapidinnovation.dev>
2024-07-11 22:56:56 +0530

    update yml file

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  // basePath: "/my-web",
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
